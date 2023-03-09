b3ac29e4789d1f9ce70e8c995d59fd1a585f714e
Gabriele <126569845+Gabrigm96@users.noreply.github.com>
2023-03-09 20:56:27 +0100

    Add files via upload

---
+++ _4/MainLesson.java
@@ -0,0 +1,17 @@
+package javabasics._4;
+
+public class MainLesson {
+    static int myAge = 26;
+    public static void main(String[] args) {
+
+        myAge = 27;
+        System.out.println("My age is = " + myAge);
+        myAge = 32;
+        System.out.println("My age is = " + myAge);
+
+
+    }
+}
+
+
+// ESEMPIO DI COME SPOSTARE LE VARIABILI E AUMENTARE IL LORO SCOPE
