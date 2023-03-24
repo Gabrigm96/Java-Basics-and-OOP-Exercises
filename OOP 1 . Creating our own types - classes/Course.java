417c52beb21e3722f12199b85538b5f79ec4db74
Gabriele <126569845+Gabrigm96@users.noreply.github.com>
2023-03-24 12:47:07 +0100

    Add files via upload

---
+++ OOP 1 . Creating our own types - classes/Course.java
@@ -0,0 +1,19 @@
+package javaoop._1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Course {
+
+    String courseName;
+    int maxStudents;
+    double qualityRatingOutOf10;
+    String studentName;
+    int studentEta;
+
+    List<Student> students = new ArrayList<>();
+
+
+
+
+}
